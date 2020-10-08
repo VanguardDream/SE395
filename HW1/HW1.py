aa095d649d8079646d8e0099f7562557d613a985
Bongsub Song <doorebong@dgist.ac.kr>
2020-10-08 18:55:39 +0900

    loader codded

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -5,11 +5,38 @@ import os
 from array import array
 from struct import *
 import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 codePath = os.path.dirname( os.path.abspath("HW1.py"))
 
-# fp_trainImage = open(codePath+'\\data\\train\\train-images.idx3-ubyte','rt',encoding='UTF8')
-# fp_trainLabel = open(codePath+'\\data\\train\\train-labels.idx1-ubyte','rt',encoding='UTF8')
+fp_trainImage = open(codePath+'\\data\\train\\train-images.idx3-ubyte','rb')
+fp_trainLabel = open(codePath+'\\data\\train\\train-labels.idx1-ubyte','rb')
+
+#Jump MNIST file header
+tmp = fp_trainImage.read(16)
+tmp = fp_trainLabel.read(8)
+
+img_load = np.zeros((28,28),dtype=int)
+label_load = np.zeros((1,10),dtype=int)
+
+# Python built in types debugging
+# print(unpack(len(tmp)*'B',tmp))
+# print(type(unpack(len(tmp)*'B',tmp)))
+# print(img_load)
+# print(label_load)
+
+bin_img = fp_trainImage.read(28*28)
+bin_label = fp_trainLabel.read(1)
+
+img_load = np.reshape(unpack(len(bin_img)*'B',bin_img),(28,28))
+label_load = int.from_bytes(bin_label,byteorder='big',signed=False)
+
+plt.imshow(img_load,cmap=cm.binary)
+plt.show()
+
+print(img_load)
+print(label_load)
 
 # img_loaded = np.zeros((28,28),dtype=int)
 # label_loaded = [[],[],[],[],[],[],[],[],[],[]]
@@ -40,74 +67,3 @@ codePath = os.path.dirname( os.path.abspath("HW1.py"))
 
 # matplotlib.pyplot.imshow(img_loaded,cmap=cm.binary)
 # matplotlib.pyplot.imshow()
-
-fp_image = open(codePath+'\\data\\train\\train-images.idx3-ubyte')
-fp_label = open(codePath+'\\data\\train\\train-labels.idx1-ubyte')
-
-img = np.zeros((28,28))                                # image resolution (28 x 28)
-
-lbl = [ [],[],[],[],[],[],[],[],[],[] ]
-d = 0
-l = 0
-index=0
-
-
-s = fp_image.read(16)	#read first 16byte
-l = fp_label.read(8)	#read first  8byte
-
-#print(s)
-#print("s_len:",len(s))
-#print(l)
-#print("l_len:",len(l))
-
-"""
-#single example - no loop
-s = fp_image.read(784)
-l = fp_label.read(1)
-print("number:",int(l[0]))
-img = np.reshape( unpack(len(s)*'B',s), (28,28))
-
-#print(img)
-plt.imshow(img,cmap = cm.binary)
-plt.show()
-"""
-
-
-k=0
-#read mnist and show character
-while True:	
-	s = fp_image.read(784)
-	l = fp_label.read(1)
-
-	if not s:
-		break;
-	if not l:
-		break;
-
-	index = int(l[0])
-	#print(k,":",index)
-	
-	#no-loop
-	img = np.reshape( unpack(len(s)*'B',s), (28,28))
-
-	"""	
-	#loop
-	for i in range(0,28):
-		for j in range(0,28):
-			#print(i,j)
-			d = s[(i*28)+j]		
-			img[i][j] = d
-			#print('%02x'%(d),end="")
-	"""	
-
-	lbl[index].append(img)
-		
-	k=k+1
-#print(img)
-
-plt.imshow(img,cmap = cm.binary)
-plt.show()
-
-print(np.shape(lbl))
-
-print("read done")
